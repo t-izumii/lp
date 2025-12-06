0978bafca72105f2c21e3b6b26ef9976b57cae82
t-izumii <120708670+t-izumii@users.noreply.github.com>
2025-12-07 03:54:04 +0900

    update

--- assets/index.js
+++ assets/index.js
@@ -18,4 +18,47 @@ window.addEventListener("load", function() {
     observer.observe(element);
   });
   document.body.classList.add("is-load");
+  const details = document.querySelectorAll(".details");
+  details.forEach((detail) => {
+    const summary = detail.querySelector("summary");
+    const answer = detail.querySelector("._answer");
+    if (!summary || !answer)
+      return;
+    if (!detail.hasAttribute("open")) {
+      answer.style.height = "0";
+    }
+    summary.addEventListener("click", (e) => {
+      e.preventDefault();
+      if (detail.hasAttribute("open")) {
+        const height = answer.scrollHeight;
+        answer.style.height = `${height}px`;
+        requestAnimationFrame(() => {
+          answer.style.height = "0";
+        });
+        answer.addEventListener(
+          "transitionend",
+          () => {
+            detail.removeAttribute("open");
+          }, {
+            once: true
+          }
+        );
+      } else {
+        detail.setAttribute("open", "");
+        const height = answer.scrollHeight;
+        answer.style.height = "0";
+        requestAnimationFrame(() => {
+          answer.style.height = `${height}px`;
+        });
+        answer.addEventListener(
+          "transitionend",
+          () => {
+            answer.style.height = "auto";
+          }, {
+            once: true
+          }
+        );
+      }
+    });
+  });
 });
